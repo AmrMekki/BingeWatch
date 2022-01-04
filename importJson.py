4498cad480f5aa0cb74ad7ad7de46bc805d1f023
mhwahdab <moustafawahdan55@gmail.com>
2022-01-04 20:24:20 +0200

    data handling

---
+++ importJson.py
@@ -0,0 +1,33 @@
+import json
+import csv
+
+for i in range(1980,2015): 
+    with open("groundtruth\\" + str(i) + ".txt", "r") as json_file:
+            string = json_file.read()
+            json_object = string.split('}')
+            json_object[len(json_object) - 1] += "}"
+            del json_object[-1]
+            json_objects = []
+            for x in json_object:
+                x = x + "}"
+                x = x.replace("ObjectId(", "")
+                x = x.replace("\n", "")
+                x = x.replace("),", ",")
+                json_objects.append(x)
+            json_list = []
+            for x in json_objects:
+                try:
+                    json_list.append(json.loads(x))           
+                except:
+                    x = x.encode("utf-16le")
+                    json_list.append(json.loads(x))  
+            data_file = open("jsonData\\"+str(i) + ".csv", 'w', newline='')
+            csv_writer = csv.writer(data_file)
+            count = 0
+            for data in json_list:
+                if count == 0:
+                    header = data.keys()
+                    csv_writer.writerow(header)
+                    count += 1
+                csv_writer.writerow(data.values())
+            data_file.close()
