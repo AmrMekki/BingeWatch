2c9127bdd3a52627e873744ec819482c1d7f1333
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2022-01-04 22:16:34 +0200

    import json

--- importJson.py
+++ importJson.py
@@ -2,7 +2,7 @@ import json
 import csv
 
 for i in range(1980,2015): 
-    with open("groundtruth\\" + str(i) + ".txt", "r") as json_file:
+    with open("groundtruth//" + str(i) + ".txt", "r") as json_file:
             string = json_file.read()
             json_object = string.split('}')
             json_object[len(json_object) - 1] += "}"
@@ -16,12 +16,8 @@ for i in range(1980,2015):
                 json_objects.append(x)
             json_list = []
             for x in json_objects:
-                try:
-                    json_list.append(json.loads(x))           
-                except:
-                    x = x.encode("utf-16le")
-                    json_list.append(json.loads(x))  
-            data_file = open("jsonData\\"+str(i) + ".csv", 'w', newline='')
+                    json_list.append(json.loads(x))             
+            data_file = open("csvData//"+str(i) + ".csv", 'w', newline='')
             csv_writer = csv.writer(data_file)
             count = 0
             for data in json_list:
