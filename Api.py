c3970e3ef7b9902792d2670164d64f4930b2b9b7
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2022-01-13 23:38:35 +0200

    search for movie title

--- Api.py
+++ Api.py
@@ -1,1 +1,15 @@
 import imdb
+moviesDB = imdb.IMDb()
+
+#print(dir(moviesDB))
+
+
+movies = moviesDB.search_movie('titanic')
+
+print('Searching for "titanic": ')
+for movie in movies:
+    title = movie['title']
+    year = movie['year']
+    print(f'{title} - {year}')
+    
+    
