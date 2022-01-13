b1eef5471bb5f99944b3032e1638652d3efc89d7
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2022-01-14 01:11:15 +0200

    built in IMDb api

--- Api.py
+++ Api.py
@@ -1,14 +1,12 @@
 import imdb
 
+movies = imdb.IMDb()
+top250 = movies.get_top250_movies()
 
 
-ia = imdb.IMDb()
-top250 = ia.get_top250_movies()
-
-# Iterate through the first 20 movies in the top 250
 for movie_count in range(0, 20):
-    # First, retrieve the movie object using its ID
-    movie = ia.get_movie(top250[movie_count].movieID)
-    # Print movie title and genres
+    
+    movie = movies.get_movie(top250[movie_count].movieID)
+    
     print(movie['title'])
     print(*movie['genres'], sep=", ")
