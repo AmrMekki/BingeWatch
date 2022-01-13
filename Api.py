1aa7c15c04f5fc654c6a1d6dfab17252b1ce6d13
NourhanElyamany <68388481+NourhanElyamany@users.noreply.github.com>
2022-01-13 23:31:59 +0200

    imdb api

--- Api.py
+++ Api.py
@@ -1,5 +1,1 @@
-import requests
-import pprint
-
-r = requests.get('https://api.themoviedb.org/3/movie/550?api_key=5f96ca821b0df5fb5a9314d566a5e5ab')
-pprint.pprint(r.json()['genres'][0])
+import imdb
